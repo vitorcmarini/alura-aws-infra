427f02005e1d4232ebdfc8bce39c856e03325d9a
vitor marini <vitorcmarini@hotmail.com>
2024-05-29 10:44:52 -0300

    first commit

--- src/main/java/com/myorg/AluraAwsInfraApp.java
+++ src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -1,40 +1,21 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
-
-import java.util.Arrays;
 
 public class AluraAwsInfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new AluraAwsInfraStack(app, "AluraAwsInfraStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        AluraVpcStack vpcStack = new AluraVpcStack(app, "Vpc");
+        AluraClusterStack clusterStack = new AluraClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+        AluraRdsStack rdsStack = new AluraRdsStack(app, "Rds", vpcStack.getVpc());
+        rdsStack.addDependency(vpcStack);
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        AluraServiceStack aluraServiceStack = new AluraServiceStack(app, "Service", clusterStack.getCluster());
+        aluraServiceStack.addDependency(clusterStack);
+        aluraServiceStack.addDependency(rdsStack);
 
         app.synth();
     }
